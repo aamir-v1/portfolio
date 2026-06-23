116c4996221a7f0805c4360c01eb8d685b9d97e5
Muhammad Aamir <aamir104512@gmail.com>
2026-06-23 16:35:21 +0500

    dummy push

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+console.log("dummy push");
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
